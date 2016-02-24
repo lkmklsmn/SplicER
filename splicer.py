63480bab23a2be0a67c09a060c8bb01468c9003f
lkmklsmn <lkmklsmn@gmail.com>
2016-02-24 11:08:14 -0600

    Update splicer.py

--- splicer.py
+++ splicer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Written (W) 2013-2016 Lukas Simon
+# Copyright (C) 2013-2016 Baylor College of Medicine
 
 import pysam
 import sys
